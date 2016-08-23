1d2094332eb87561213db854a063273f5c5a0ef8
Deepak Vishwakarma <deepak.m.shrma@gmail.com>
2016-08-23 12:48:31 +0800

    updated script to consume -v

--- nodejs-installer.sh
+++ nodejs-installer.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
-version=$1
-fversion=$1
-isSpecific='^[0-9\.]+$'
-if [[ $version =~ $isSpecific ]] ; then
-   fversion=v$version
+# Use -gt 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to go with it).
+# Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
+# some arguments don't have a corresponding value to go with it such
+# as in the --default example).
+# note: if this is set to -gt 0 the /etc/hosts part is not recognized ( may be a bug )
+while [[ $# -gt 1 ]]
+do
+key="$1"
+case $key in
+    -v|--version)
+    VERSION="$2"
+    shift # past argument
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+IS_SPECIFIC='^[0-9\.]+$'
+if [[ $VERSION =~ $IS_SPECIFIC ]] ; then
+   VERSION=v$VERSION
   else
-  case $version in
+  case $VERSION in
     lts )
-    fversion='v4.4.7' ;;
+    VERSION='v4.4.7' ;;
     latest )
-    fversion='v6.2.2' ;;
+    VERSION='v6.4.0' ;;
     *) 
-    fversion='v4.4.7' ;;
+    VERSION='v4.4.7' ;;
   esac
 fi
 echo "removing old node..."
@@ -22,14 +43,14 @@ echo "old node remove done!!"
 echo "Changing owner of the folder"
 sudo chown $(whoami):$(whoami) /opt/
 cd /opt
-echo "Downloading node version: $fversion"
+echo "Downloading node version: $VERSION"
 sleep 3 
-wget "https://nodejs.org/dist/$fversion/node-$fversion-linux-x64.tar.gz"
+wget "https://nodejs.org/dist/$VERSION/node-$VERSION-linux-x64.tar.gz"
 echo "Downloading completed"
 sleep 3
-tar -xvzf node-$fversion-linux-x64.tar.gz
+tar -xvzf node-$VERSION-linux-x64.tar.gz
 rm -rf node
-ln -s node-$fversion-linux-x64 node
+ln -s node-$VERSION-linux-x64 node
 echo "Go Back to Home"
 cd ~
 echo "Updating .bashrc file"
